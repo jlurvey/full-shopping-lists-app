badefafe37396da030372548c3df76ba98cf0ef6
Joseph Lurvey <joeylurvey@gmail.com>
2024-01-31 21:03:38 -0500

    Add Store model

---
+++ models.py
@@ -0,0 +1,25 @@
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+
+from config import db
+
+# Models go here!
+class Item(db.Model, SerializerMixin):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    category = db.Column(db.String)
+    need = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f'<Item {self.id}, {self.name}, Category: {self.category}, Need: {self.need}>'
+
+class Store(db.Model, SerializerMixin):
+    __tablename__ = 'stores'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<Store {self.id}, {self.name}>'
