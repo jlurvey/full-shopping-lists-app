1527e31edf9c6ce2757b1b7703dadb43d1596f6d
Joseph Lurvey <joeylurvey@gmail.com>
2024-02-03 08:55:13 -0500

    Reset database

--- models.py
+++ models.py
@@ -8,9 +8,13 @@ class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True, nullable=False)
-    category = db.Column(db.String)
-    need = db.Column(db.Boolean)
+    name = db.Column(db.String, unique=True, nullable=False) #needs validation
+    category = db.Column(db.String) #needs validation
+    need = db.Column(db.Boolean) #needs validation
+
+    #relationship mapping item to related stores
+    stores= db.relationship(
+        'Store', back_populates = 'item', cascade='all, delete-orphan')
 
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, Category: {self.category}, Need: {self.need}>'
@@ -19,7 +23,31 @@ class Store(db.Model, SerializerMixin):
     __tablename__ = 'stores'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False) #needs validation
+
+    #relationship mapping store to related items
+    items=db.relationship(
+        'Item', back_populates = 'store', cascade ='all, delete-orphan')
 
     def __repr__(self):
         return f'<Store {self.id}, {self.name}>'
+    
+class Note(db.model, SerializerMixin):
+    __tablename__ = 'notes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String) #needs validation
+
+    #fk for item id
+    item_id= db.Column(db.Integer, db.ForeignKey('items.id'))
+    #fk for store id
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    
+    #relationship mapping note to related item
+    item = db.relationship('Item', back_populates='notes')
+
+    #relationship mapping note to related store
+    store = db.relationship('Store', back_populates='notes')
+
+    def __repr__(self):
+        return f'<Note {self.id}, Item name: {self.item.name}, Store name: {self.store.name}, {self.description}>'
