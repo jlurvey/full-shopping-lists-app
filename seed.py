c0f5f75148d070ec1dccc63c9fdfec0d5b95652d
Joseph Lurvey <joeylurvey@gmail.com>
2024-02-04 09:27:20 -0500

    Initialize data seeding for items table

--- seed.py
+++ seed.py
@@ -1,19 +1,49 @@
 #!/usr/bin/env python3
 
 # Standard library imports
-from random import randint, choice as rc
+from random import random, randint, choice as rc
 
 # Remote library imports
 from faker import Faker
+from faker.providers import BaseProvider
 
 # Local imports
 from app import app
 from models import db, Item, Store, Note
 
-#with app.app_context
-
 if __name__ == '__main__':
+    
     fake = Faker()
+    
     with app.app_context():
+        
         print("Starting seed...")
-        # Seed code goes here!
+        print("Creating items")
+        
+        #items have unique names
+        items = []
+        names = []
+        categories = ['grocery store','hardware store','pharmacy store','convenience store','department store']
+
+        for i in range(20):
+
+            name = fake.word()
+            while name in names:
+                name = fake.word()
+                names.append(name)
+            
+            item = Item(
+                name=name,
+                category=random.choice(categories),
+                need=fake.boolean()
+            )
+
+            items.append(item)
+
+        db.session.add_all(items)
+
+
+
+
+                
+
