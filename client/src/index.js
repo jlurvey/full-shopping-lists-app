f312c505b3e001a9055c67cf6c977309cbb557b8
Joseph Lurvey <joeylurvey@gmail.com>
2024-02-15 20:59:03 -0500

    Add redux-store and items, notes, and stores slice files.

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import App from "./components/App";
+import { Provider } from 'react-redux'
+import store from './store'
 import "./index.css";
 import { createRoot } from "react-dom/client";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+)
