1bda69dbe71e5ea2f2121c7dad44227201ee3b1d
Joseph Lurvey <joeylurvey@gmail.com>
2024-02-17 11:31:38 -0500

    Setup file as entry point

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,20 @@
+//source/index.js
+
 import React from "react";
 import App from "./components/App";
 import { Provider } from 'react-redux'
 import store from './store'
 import "./index.css";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from 'react-router-dom'
+
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
     <Provider store={store}>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </Provider>,
-)
+);
