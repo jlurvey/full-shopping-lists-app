ea2ec1e0c6f524bf902f25e71e9a40d91a15e645
Joey <joeylurvey@gmail.com>
2024-05-16 20:37:06 -0400

    Update CategoriesSlice for Categories to be a full object with full CRUD actions

--- client/src/features/categories/categoriesSlice.js
+++ client/src/features/categories/categoriesSlice.js
@@ -2,9 +2,10 @@
 
 import { createSlice, createAsyncThunk, createEntityAdapter } from '@reduxjs/toolkit'
 import axios from 'axios'
+import { fetchItems } from "../items/itemsSlice";
 
 const categoriesAdapter = createEntityAdapter({
-    selectId: category => category.name
+    selectId: category => category.id
 });
 
 const initialState = categoriesAdapter.getInitialState({
@@ -17,6 +18,37 @@ const API_URL = 'http://localhost:5555'
 export const fetchCategories = createAsyncThunk('categories/fetchCategories', async () => {
     const resp = await axios.get(`${API_URL}/categories`)
     return resp.data
+});
+
+export const addCategory = createAsyncThunk('categories/addCategory', async (initialCategory) => {
+    try {
+        const resp = await axios.post(`${API_URL}/categories`, initialCategory)
+        return resp.data
+    } catch (error) {
+        console.error(error.response.data);
+        throw error.response.data;
+    }
+});
+
+export const updateCategory = createAsyncThunk('categories/updateCategory', async ({ categoryId, updatedCategory }) => {
+    try {
+        const resp = await axios.patch(`${API_URL}/categories/${categoryId}`, updatedCategory);
+        return resp.data;
+    } catch (error) {
+        console.error(error.response.data);
+        throw error.response.data;
+    }
+});
+
+export const deleteCategory = createAsyncThunk('categories/deleteCategory', async (categoryId, { dispatch, getState }) => {
+    try {
+        await axios.delete(`${API_URL}/categories/${categoryId}`);
+        await dispatch(fetchItems());
+        return categoryId
+    } catch (error) {
+        console.error(error.response.data);
+        throw error.response.data;
+    }
 });
 
 const categoriesSlice = createSlice({
@@ -37,9 +69,16 @@ const categoriesSlice = createSlice({
                 state.status = 'failed';
                 state.error = action.error.message;
             })
+            .addCase(addCategory.fulfilled, categoriesAdapter.addOne)
+            .addCase(addCategory.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.payload;
+            })
+            .addCase(updateCategory.fulfilled, categoriesAdapter.upsertOne)
+            .addCase(deleteCategory.fulfilled, categoriesAdapter.removeOne);
     },
 });
 
 export default categoriesSlice.reducer
 
-export const { selectAll: selectAllCategories } = categoriesAdapter.getSelectors(state => state.categories);
+export const { selectAll: selectAllCategories, selectById: selectCategoryById } = categoriesAdapter.getSelectors(state => state.categories);
