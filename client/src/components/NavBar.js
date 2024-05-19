0c0eecf799666b372cc784e18a77e90d7c08832c
Joey <joeylurvey@gmail.com>
2024-05-19 13:19:03 -0400

    Add Home and Categories to NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,9 +4,11 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
     return (
         <div className="navbar">
+            <NavLink to='/' exact activeClassName="active" className="NavLink">Home</NavLink>
             <NavLink to='/items' exact activeClassName="active" className="NavLink">Items</NavLink>
             <NavLink to='/stores' exact activeClassName="active" className="NavLink">Stores</NavLink>
             <NavLink to='/lists' exact activeClassName="active" className="NavLink">Lists</NavLink>
+            <NavLink to='/categories' exact activeClassName="active" className="NavLink">Categories</NavLink>
         </div>
     );
 }
