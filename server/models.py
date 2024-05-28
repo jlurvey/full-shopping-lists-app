38b608b66c4439a0d6679de9c7dcdc49c8b1e4e8
Joey <joeylurvey@gmail.com>
2024-05-28 19:55:27 -0400

    Correct check_password return statement

--- server/models.py
+++ server/models.py
@@ -31,8 +31,7 @@ class User(db.Model, SerializerMixin):
         self._password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
 
     def check_password(self, password):
-        if not bcrypt.check_password_hash(self._password_hash, password.encode("utf-8")):
-            raise ValueError("Password is incorrect, please try again")
+        return bcrypt.check_password_hash(self._password_hash, password.encode("utf-8"))
 
     @validates("email")
     def validate_email(self, key, email):
