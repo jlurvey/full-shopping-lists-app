62245af1fba5cd04fe594c6d5ed0868f7e2aa2bf
Joey <joeylurvey@gmail.com>
2024-05-31 22:04:32 -0400

    Complete usersSlice and add to store.js

--- client/src/features/users/usersSlice.js
+++ client/src/features/users/usersSlice.js
@@ -15,12 +15,106 @@ const initialState = usersAdapter.getInitialState({
 
 const API_URL = 'http://localhost:5555'
 
-export const signup = createAsyncThunk('users/signup', async (initialUser) => {
+export const signup = createAsyncThunk('users/signup', async (userData) => {
     try {
-        const resp = await axios.post(`${API_URL}/signup`, initialUser)
+        const resp = await axios.post(`${API_URL}/signup`, userData)
         return resp.data
     } catch (error) {
         console.error(error.response.data);
         throw error.response.data;
     }
 });
+
+export const login = createAsyncThunk('users/login', async (userData) => {
+    try {
+        const resp = await axios.post(`${API_URL}/login`, userData)
+        return resp.data
+    } catch (error) {
+        console.error(error.response.data);
+        throw error.response.data;
+    }
+});
+
+export const checkSession = createAsyncThunk('users/checkSession', async () => {
+    try {
+        const resp = await axios.get(`${API_URL}/check_session`);
+        return resp.data;
+    } catch (error) {
+        console.error(error.response.data);
+        throw error.response.data;
+    }
+});
+
+export const logout = createAsyncThunk('users/logout', async () => {
+    try {
+        await axios.delete(`${API_URL}/delete`);
+        return null
+    } catch (error) {
+        console.error(error.response.data);
+        throw error.response.data;
+    }
+});
+
+const usersSlice = createSlice({
+    name: 'users',
+    initialState,
+    reducers: {
+        setCurrentUser: (state, action) => {
+            state.currentUser = action.payload
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(signup.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(signup.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                usersAdapter.addOne(state, action.payload);
+                state.currentUser = action.payload;
+            })
+            .addCase(signup.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.error.message;
+            })
+            .addCase(login.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(login.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.currentUser = action.payload;
+            })
+            .addCase(login.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.error.message;
+            })
+            .addCase(checkSession.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(checkSession.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.currentUser = action.payload;
+            })
+            .addCase(checkSession.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.error.message;
+            })
+            .addCase(logout.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(logout.fulfilled, (state) => {
+                state.status = 'succeeded';
+                state.currentUser = null;
+            })
+            .addCase(logout.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.error.message;
+            });
+    },
+});
+
+export default usersSlice.reducer;
+
+export const { selectAll: selectAllUsers, selectById: selectUserById } = usersAdapter.getSelectors((state) => state.users);
+
+export const { setCurrentUser } = usersSlice.actions
--- client/src/store.js
+++ client/src/store.js
@@ -5,6 +5,7 @@ import itemsReducer from './features/items/itemsSlice'
 import storesReducer from './features/stores/storesSlice'
 import notesReducer from './features/notes/notesSlice'
 import categoriesReducer from './features/categories/categoriesSlice'
+import usersReducer from './features/users/usersSlice'
 
 const store = configureStore({
     reducer: {
@@ -12,6 +13,7 @@ const store = configureStore({
         stores: storesReducer,
         notes: notesReducer,
         categories: categoriesReducer,
+        users: usersReducer,
     }
 });
 
