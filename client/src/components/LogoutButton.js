331731360d821b8afbd621667209b70ac41f389a
Joey <joeylurvey@gmail.com>
2024-06-02 14:24:45 -0400

    Implement resetState and useHistory functionality

--- client/src/components/LogoutButton.js
+++ client/src/components/LogoutButton.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { logout } from "../features/users/usersSlice";
+import { useHistory } from "react-router-dom";
+import { logout, resetState } from "../features/users/usersSlice";
 
 function LogoutButton() {
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const handleLogout = () => {
-    dispatch(logout());
+    dispatch(logout()).then(() => {
+      dispatch(resetState());
+      history.push('/')
+    });
   };
 
   return (
